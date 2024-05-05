9fd5c568262d8414868576df16e2ba703a10d71d
focsit <f0csit@mail.ru>
2024-05-05 17:55:29 +0300

    Started API requests

---
+++ frontend/src/features/modals/CreateBrand.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
+
+const CreateBrand = ({show, onHide}) => {
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить бренд
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control
+                        placeholder={"Введите название бренда"}
+                    />
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={onHide}>Добавить</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default CreateBrand;
---
+++ frontend/src/features/modals/CreateType.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
+
+const CreateType = ({show, onHide}) => {
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить тип
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control
+                        placeholder={"Введите название типа"}
+                    />
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={onHide}>Добавить</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default CreateType;
